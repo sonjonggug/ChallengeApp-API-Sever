18cce73f131d81f1cba44911792570e601382986
손종국 <kyky7852@naver.com>
2023-04-11 13:04:52 +0900

    cors 에러 해결중 ( WebConfig 작성해서 모든 요청에 대해 허용해줬는데도 자꾸 에러남 .. )

--- CheckLinJi-API/src/main/java/com/upside/api/config/WebConfig.java
+++ CheckLinJi-API/src/main/java/com/upside/api/config/WebConfig.java
@@ -1,22 +1,36 @@
 package com.upside.api.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import lombok.AllArgsConstructor;
-
-@AllArgsConstructor
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 	
-	
-	
-	
-  @Override
-  public void addCorsMappings(CorsRegistry registry) {
-    registry.addMapping("/**")
-        .allowedOrigins("http://localhost:3000");
-  }
- 
+	   @Override
+	    public void addCorsMappings(CorsRegistry registry) {
+	        registry.addMapping("/**")
+	                .allowedOriginPatterns("*")
+	                .allowedMethods("*")
+	                .allowedHeaders("*")
+	                .allowCredentials(true)
+	                .maxAge(3600);
+	    }
+
+	    @Bean
+	    public CorsFilter corsFilter() {
+	        CorsConfiguration config = new CorsConfiguration();
+	        config.setAllowCredentials(true);
+	        config.addAllowedOriginPattern("*");
+	        config.addAllowedHeader("*");
+	        config.addAllowedMethod("*");
+	        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	        source.registerCorsConfiguration("/**", config);
+	        return new CorsFilter(source);
+	    }
+
 }
