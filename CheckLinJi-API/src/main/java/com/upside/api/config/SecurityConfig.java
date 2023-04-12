463cd5d1bb7e45c2bb7d2fe1d0bd0e8d87c89bfd
손종국 <kyky7852@naver.com>
2023-04-12 18:54:29 +0900

    시큐리티 cors 설정 추가 ( security가 먼저 요청을 받고 있어서 에러 났었음)

--- CheckLinJi-API/src/main/java/com/upside/api/config/SecurityConfig.java
+++ CheckLinJi-API/src/main/java/com/upside/api/config/SecurityConfig.java
@@ -12,6 +12,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import lombok.RequiredArgsConstructor;
 @Configuration
@@ -30,15 +33,18 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf().disable() // rest api이므로 csrf 보안 미사용
-        .httpBasic().disable() // rest api이므로 기본설정 미사용        
+        .cors().configurationSource(corsConfigurationSource()) // cors 설정 
+        .and()
+        .httpBasic().disable() // 기본 인증창(rest api이므로 기본설정 미사용)        
         .formLogin().disable() // rest api 폼 로그인 인증 방식을 비활성화
         .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS); // jwt로 인증하므로 세션 미사용        
         http.authorizeHttpRequests() // HTTP 요청에 대한 인가 규칙을 설정        		
         		.requestMatchers("/api/members/sign/**").permitAll()  // login 없이 접근 허용 하는 URL
-        		.requestMatchers("/api/members/login/**").permitAll()  
-        		.requestMatchers("/api/members/validateDuplicated/**").permitAll()  
-        		.requestMatchers("/api/members/refreshToken/**").permitAll()  
+        		.requestMatchers("/api/members/login/**").permitAll()
+        		.requestMatchers("/api/members/email/**").permitAll()        		
+        		.requestMatchers("/api/members/validateDuplicated/**").permitAll()
         		.requestMatchers("/api/social/login/**").permitAll()
+        		.requestMatchers("/api/members/refreshToken/**").permitAll()          		
         		.requestMatchers("/api/external/**").permitAll()  // login 없이 접근 허용 하는 URL
 //        		.requestMatchers("/api/challenge/join/**").permitAll()
 //        		.requestMatchers("/api/challenge/submit/**").permitAll()        		
@@ -50,7 +56,27 @@ public class SecurityConfig {
 
         return http.build();
     }
-              
+     
+    
+    // CORS 허용 적용
+    
+    @Bean 
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+//        configuration.addAllowedOrigin("*");
+        configuration.addAllowedOrigin("http://localhost:8080");
+        configuration.addAllowedOrigin("http://localhost:9999");
+        configuration.addAllowedOrigin("http://localhost:3000");
+        configuration.addAllowedHeader("*"); // 모든 헤더 허용
+        configuration.addAllowedMethod("*"); // 모든 메서드허용
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+    
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
--- CheckLinJi-API/src/main/java/com/upside/api/config/WebConfig.java
+++ CheckLinJi-API/src/main/java/com/upside/api/config/WebConfig.java
@@ -1,36 +1,21 @@
-package com.upside.api.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
-	
-	   @Override
-	    public void addCorsMappings(CorsRegistry registry) {
-	        registry.addMapping("/**")
-	                .allowedOriginPatterns("*")
-	                .allowedMethods("*")
-	                .allowedHeaders("*")
-	                .allowCredentials(true)
-	                .maxAge(3600);
-	    }
-
-	    @Bean
-	    public CorsFilter corsFilter() {
-	        CorsConfiguration config = new CorsConfiguration();
-	        config.setAllowCredentials(true);
-	        config.addAllowedOriginPattern("*");
-	        config.addAllowedHeader("*");
-	        config.addAllowedMethod("*");
-	        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-	        source.registerCorsConfiguration("/**", config);
-	        return new CorsFilter(source);
-	    }
-
-}
+//package com.upside.api.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//import lombok.RequiredArgsConstructor;
+//
+//@RequiredArgsConstructor
+//@Configuration
+//public class WebConfig implements WebMvcConfigurer {
+//	
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry){
+//        registry.addMapping("/**")
+////                .allowedOrigins("*")
+//        		.allowedOrigins("http://localhost:3000","http://localhost:8080","http://localhost:9999") // 두개 Origin(출처) 허용
+//                .allowedMethods("*") // 모든 메소드타입 ( GET , POST 등.. ) 허용
+//                .allowedHeaders("*"); // 모든 헤더 허용
+//    }
+//}
